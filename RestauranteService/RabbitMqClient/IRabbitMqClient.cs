e1f1dd07a0cf93755a30ceb0d6c0e269c4712a11
Andre1999Lopes <andrelopesbr1999@gmail.com>
2023-08-23 19:16:21 -0300

    Projeto finalizado

---
+++ RestauranteService/RabbitMqClient/IRabbitMqClient.cs
@@ -0,0 +1,9 @@
+﻿using RestauranteService.Dtos;
+
+namespace RestauranteService.RabbitMqClient
+{
+    public interface IRabbitMqClient
+    {
+        public void PublicarRestaurante(RestauranteReadDto restaurante);
+    }
+}
